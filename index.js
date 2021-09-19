9cf1f1f9ce3d7635f3009e5c4e4d007871521827
MatheusVicente7 <44214210+MatheusVicente7@users.noreply.github.com>
2021-09-18 21:04:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+	else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	}
+	else if (commandName === 'user') {
+		await interaction.reply(`Sua tag: ${interaction.user.tag}\nSeu id: ${interaction.user.id}`);
+	}
+	else if (commandName === 'hello') {
+		await interaction.reply('world!');
+	}
+	else if (commandName === 'dj') {
+		await interaction.reply('Na Irlanda do Norte eu sou idolo !');
+	}
+});
+// Login to Discord with your client's token
+client.login(token);
