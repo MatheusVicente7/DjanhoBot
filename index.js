ee87ec279772d9ec216255594860ec0e837c4c81
MatheusVicente7 <44214210+MatheusVicente7@users.noreply.github.com>
2021-09-18 21:35:10 -0300

    add interaction listeners

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+// eslint-disable-next-line no-unused-vars
+const { token, prefix } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -9,6 +10,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.once('ready', () => {
 	console.log('Ready!');
 });
+// Listen client interaction
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
@@ -30,5 +32,32 @@ client.on('interactionCreate', async interaction => {
 		await interaction.reply('Na Irlanda do Norte eu sou idolo !');
 	}
 });
+// Reads client message
+client.on('message', async message => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	// eslint-disable-next-line no-undef
+	const serverQueue = queue.get(message.guild.id);
+
+	if (message.content.startsWith(`${prefix}play`)) {
+		// eslint-disable-next-line no-undef
+		execute(message, serverQueue);
+		return;
+	}
+	else if (message.content.startsWith(`${prefix}skip`)) {
+		// eslint-disable-next-line no-undef
+		skip(message, serverQueue);
+		return;
+	}
+	else if (message.content.startsWith(`${prefix}stop`)) {
+		// eslint-disable-next-line no-undef
+		stop(message, serverQueue);
+		return;
+	}
+	else {
+		message.channel.send('You need to enter a valid command!');
+	}
+});
+
 // Login to Discord with your client's token
 client.login(token);
