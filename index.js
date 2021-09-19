3a0f2dbed662cd59a67810ea7c85d34cd28ada01
MatheusVicente7 <44214210+MatheusVicente7@users.noreply.github.com>
2021-09-19 00:56:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,77 +5,69 @@ const ytdl = require('ytdl-core');
 const { token, prefix } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
+const queue = new Map();
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
-// Listen client interaction
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	}
-	else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	}
-	else if (commandName === 'user') {
-		await interaction.reply(`Sua tag: ${interaction.user.tag}\nSeu id: ${interaction.user.id}`);
-	}
-	else if (commandName === 'hello') {
-		await interaction.reply('world!');
-	}
-	else if (commandName === 'dj') {
-		await interaction.reply('Na Irlanda do Norte eu sou idolo !');
-	}
+client.once('reconnecting', () => {
+	console.log('Reconnecting!');
 });
-// Reads client message
+
+client.once('disconnect', () => {
+	console.log('Disconnect!');
+});
+
 client.on('message', async message => {
 	if (message.author.bot) return;
 	if (!message.content.startsWith(prefix)) return;
+
 	const serverQueue = queue.get(message.guild.id);
 
 	if (message.content.startsWith(`${prefix}play`)) {
-		// eslint-disable-next-line no-undef
+		execute(message, serverQueue);
+		return;
+	}
+	if (message.content.startsWith(`${prefix}dj`)) {
+		execute(message, serverQueue);
+		return;
+	}
+	if (message.content.startsWith('dj')) {
 		execute(message, serverQueue);
 		return;
 	}
 	else if (message.content.startsWith(`${prefix}skip`)) {
-		// eslint-disable-next-line no-undef
 		skip(message, serverQueue);
 		return;
 	}
 	else if (message.content.startsWith(`${prefix}stop`)) {
-		// eslint-disable-next-line no-undef
 		stop(message, serverQueue);
 		return;
 	}
 	else {
-		message.channel.send('You need to enter a valid command!');
+		message.channel.send('Você precisa utilizar um comando válido seu lazarento!');
 	}
 });
-
-const queue = new Map();
 
 async function execute(message, serverQueue) {
 	const args = message.content.split(' ');
 
 	const voiceChannel = message.member.voice.channel;
-
 	if (!voiceChannel) {
 		return message.channel.send(
-			'Você precisa estar em um Canal de Voz para escutar uma música!');
+			'Você precisa estar em um canal de aúdio para escutar música!',
+		);
 	}
 	const permissions = voiceChannel.permissionsFor(message.client.user);
 	if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
 		return message.channel.send(
-			'O Djanho precisa de permissão para entrar e falar em seu canal de voz');
+			'O Djanho precisa de permissão para acessar o canal',
+		);
 	}
-	// ytdl safes info video into a object
+
 	const songInfo = await ytdl.getInfo(args[1]);
 	const song = {
 		title: songInfo.videoDetails.title,
@@ -83,27 +75,24 @@ async function execute(message, serverQueue) {
 	};
 
 	if (!serverQueue) {
-		// Creates a contract for the queue
-		const queueContract = {
-			textChannel : message.channel,
-			voiceChannel : voiceChannel,
-			connection : null,
-			songs : [],
+		const queueContruct = {
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
 			volume: 5,
-			playing : true,
+			playing: true,
 		};
-		// Setting the queue using our contract
-		queue.set(message.guild.id, queueContract);
 
-		// Pushing the song to our songs array
-		queueContract.songs.push(song);
+		queue.set(message.guild.id, queueContruct);
+
+		queueContruct.songs.push(song);
 
 		try {
 			const connection = await voiceChannel.join();
-			queueContract.connection = connection;
+			queueContruct.connection = connection;
 
-			play(message.guild, queueContract.songs[0]);
-
+			play(message.guild, queueContruct.songs[0]);
 		}
 		catch (err) {
 			console.log(err);
@@ -113,9 +102,31 @@ async function execute(message, serverQueue) {
 	}
 	else {
 		serverQueue.songs.push(song);
-		console.log(serverQueue.songs);
-		return message.channel.send(`${song.title} foi adicionada a playlist`);
+		return message.channel.send(`${song.title} foi adicionado a lista!`);
 	}
+}
+
+function skip(message, serverQueue) {
+	if (!message.member.voice.channel) {
+		return message.channel.send(
+			'Você precisa estar em um canal de aúdio para escutar música!',
+		);
+	}
+	if (!serverQueue) {return message.channel.send('Não tem música tocando no momento');}
+	serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+	if (!message.member.voice.channel) {
+		return message.channel.send(
+			'Você precisa estar em um canal de aúdio para escutar música!',
+		);
+	}
+
+	if (!serverQueue) {return message.channel.send('Não tem música tocando no momento');}
+
+	serverQueue.songs = [];
+	serverQueue.connection.dispatcher.end();
 }
 
 function play(guild, song) {
@@ -134,11 +145,6 @@ function play(guild, song) {
 		})
 		.on('error', error => console.error(error));
 	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-	serverQueue.textChannel.send(`Música seguinte: **${song.title}**`);
-
-
+	serverQueue.textChannel.send(`: **${song.title}**`);
 }
-
-
-// Login to Discord with your client's token
 client.login(token);
